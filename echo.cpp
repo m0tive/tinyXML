e2273d9b7bf616f312e64f8485fc409d81b824a0
leethomason <leethomason>
2002-03-21 00:13:36 +0000

    Adding echotest and hamlet test xml

---
+++ echo.cpp
@@ -0,0 +1,29 @@
+#include "tinyxml.h"
+#include <time.h>
+
+int main( int argc, char* argv[] )
+{
+	if ( argc < 2 )
+	{
+		printf( "Loads a file, then writes it back out. Times both steps.\n" );
+		printf( "Usage: echo inputfile\n" );
+		return 1;
+	}
+
+	TiXmlDocument doc( argv[1] );
+	
+	time_t start, read, wrote;
+
+	start = clock();
+	doc.LoadFile();
+	read = clock();
+	doc.SaveFile( "echotest.xml" );
+	wrote = clock();
+
+	printf( "Read: %fms  Write: %fms\n", double( read-start ) / double( CLOCKS_PER_SEC ),
+										 double( wrote-read ) / double( CLOCKS_PER_SEC ) );
+	
+
+
+	return 0;
+}
